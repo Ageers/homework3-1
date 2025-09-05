f5fbd469bea465da6df61054f536266a7176024c
Ageers <Ageers@mail.ru>
2025-09-05 19:59:01 +0300

    add second task

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+/* // Сумма от 1 до N Ввод: одно целое N (N ≥ 1). Вывести: сумму 1 + 2 + … + N.
         int n = sc.nextInt();
         int res = 0;
         for (int i = 0; i <= n; i++){
@@ -14,5 +14,13 @@ public class Main {
         System.out.println(res);
 
 
+*/
+        //2 Сумма квадратов нечётных ≤ N Ввод: N. Вывести: 1² + 3² + 5² + ... ≤ N.
+        int n = sc.nextInt();
+        int res = 0;
+        for (int i = 1; i <= n; i = i + 2){
+            res += Math.pow(i, 2);
+        }
+        System.out.println(res);
     }
 }
